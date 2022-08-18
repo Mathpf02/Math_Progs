916aa9243a8499e113307bb4870344201d11159b
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-08-18 16:17:55 -0300

    nov.atu

---
+++ LOGIN/PHP/Login.php
@@ -0,0 +1,28 @@
+ <!DOCTYPE html>
+ <html lang="pt-br">
+
+ <head>
+     <meta charset="UTF-8">
+     <title>CoDe-Cadastro</title>
+     <link rel="stylesheet" href="../CSS/St_Log.css">
+ </head>
+
+ <body>
+    <div>
+     <h1> CoDe</h1>
+     <h3>Sistema de Cadastro</h3>
+
+     <form method="POST" action="proc_Login">
+
+         <input type="text" name="matricula" placeholder="Matricula"><br><br>
+         <input type="text" name="senha" placeholder="Senha"><br><br>
+
+         <input type="submit" value="ENTRAR"><br>
+         <a href="">Ainda não esta cadastrado? <strong>CADASTRE-SE</strong> </a>
+
+     </form>
+    </div>
+
+ </body>
+
+ </html>
