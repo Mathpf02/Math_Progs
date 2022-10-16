5b2d27c164baefaf1df86a96181b4fb03eb4c10d
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-10-16 15:30:51 -0300

    log.com

---
+++ Ativ.II_Prog/log.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log.Site</title>
+</head>
+<body>
+    <div class="ped_form">
+        <form action="" method="POST">
+
+        E-MAIL:
+        <input type="email"  id="f_email" name="email" placeholder="Digite seu email"><br>
+        SENHA:
+        <input type="password"  id="f_senha" name="senha" placeholder="Digite sua senha"><br>
+        <input type="submit" value="ENTRAR">
+
+        </form>
+
+    </div>
+</body>
+</html>
