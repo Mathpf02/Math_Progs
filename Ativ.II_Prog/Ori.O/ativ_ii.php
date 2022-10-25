d73cbc21e07da2bd84512c1c85f0ea0edb779a76
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-10-25 18:13:21 -0300

    Create ativ_ii.php

---
+++ Ativ.II_Prog/Ori.O/ativ_ii.php
@@ -0,0 +1,20 @@
+<?php
+
+class calculo
+{
+    private $altu;
+    private $larg;
+
+    public function __construct()
+    {
+        $this->altura = 1;
+        $this->largura = 1;
+    }
+    public function setLargura($larg)
+    {
+        if ($larg >= 20 && $larg <= 0) {
+            $this->altura = $larg;
+        }
+    }
+
+}
