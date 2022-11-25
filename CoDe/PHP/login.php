69aed8a4c06ba775bc85819d425f94fc6418c261
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-11-25 17:22:36 -0300

    new

--- CoDe/PHP/login.php
+++ CoDe/PHP/login.php
@@ -1,21 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Login-CODE</title>
     <link rel="stylesheet" href="../CSS/St_log.css">
 </head>
-<body>
-<div id="corpo-form">
-    <h1>CoDe</h1>
-    <h3>Sistema de Empréstimo</h3>
-    <form method= "POST" action="proc_login.php">
-        <input type="matricula" placeholder="DIGITE SUA MATRÍCULA"><br>
-        <input type="senha" placeholder="DIGITE SUA SENHA"><br>
-        <input type="submit" value="ENTRAR"><br><br>
-        <a href="cadastro.html"> Ainda não é cadastrado ? <strong>CADASTRE-SE!</strong></a>
 
-    </form>
-</div>
+<body>
+    <div class="logo">MAXLEADS</div>
+    <div class="container">
+        <div class="esquerda">
+            <div class="superior">
+                <p>Sistema de</p>
+                <h2>EMPRESTIMO</h2>
+            </div>
+
+            <form method="POST" action="" class="form">
+                <label for="email">E-MAIL OU MATRICULA</label>
+                <input type="text" class="email"><br>
+                <label for="password">SENHA</label>
+                <input type="password" class="password"><br>
+
+                <span class="esquecer" style="color:blue;">
+                    <a href=""> Esqueceu sua senha? </a>
+                </span>
+
+                <button>ENTRAR</button>
+                <span class="Cadastrar" style="color:blue;">
+                    <a href=""> Precisando de uma conta? <strong>Registre-se</strong> </a>
+                </span>
+            </form>
+
+        </div>
+        <div class="direita">
+            <img src="../IMG/qrcode_TCC.jpg" alt="">
+            <h2>Leia o meu TCC</h2>
+            <p>Escaneie isto com o <b>Google Leans atráves da câmera do celular</b> para ler o poder ler o documento.</p>
+
+        </div>
+
+    </div>
+
 </body>
+
 </html>
