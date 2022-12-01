5bb03ae6194869bc8cc7ce06d2dc68d59693beac
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-11-30 23:31:14 -0300

    lkl

---
+++ CoDe/PHP/recuperar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CoDe: RECUPERA</title>
+    <link rel="stylesheet" href="../CSS/Prin_CoDe/St_rec.css">
+</head>
+
+<body>
+    <div class="conteiner">
+        <div class="esquerda">
+            <div class="superior">
+                <p>CoDe:</p>
+                <strong>
+                    <h2>RECUPERAR SENHA</h2>
+                </strong>
+            </div>
+
+            <form method="POST" action="" class="form">
+                <label for="email">E-MAIL</label>
+                <input type="text" id="email"><br>
+
+                <button type="submit"  href="../HTML/Inicio.html">SOLICITAR RECUPERAÇÃO</button><br>
+            </form>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ CoDe/PHP/troca_senha.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CoDe: LOGIN</title>
+    <link rel="stylesheet" href="../CSS/Prin_CoDe/St_Log.css">
+</head>
+
+<body>
+    <div class="conteiner">
+        <div class="esquerda">
+            <div class="superior">
+                <p>Sistema de</p>
+                <strong>
+                    <h2>EMPRESTIMO</h2>
+                </strong>
+            </div>
+
+            <form method="POST" action="" class="form">
+                <label for="senha">SENHA</label>
+                <input type="password" id="senha">
+                <label for="senha"> CONFIRMAR SENHA</label>
+                <input type="password" id="senha">
+
+                <button type="submit"  href="../HTML/Inicio.html">SALVAR</button><br>
+            </form>
+
+    </div>
+
+</body>
+
+</html>
