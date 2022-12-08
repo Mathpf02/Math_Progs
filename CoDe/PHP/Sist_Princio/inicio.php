501f827a883db8848801582989569555bbc918c6
Matheus Font <110690315+Mathpf02@users.noreply.github.com>
2022-12-08 10:13:11 -0300

    klj

--- CoDe/PHP/Sist_Princio/inicio.php
+++ CoDe/PHP/Sist_Princio/inicio.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../../CSS/Seg_CoDe/St_Inicio.css"> 
+    <link rel="stylesheet" href="../../CSS/Seg_CoDe/St_Inicio.css">
     <title>CoDe: PROJETO</title>
- 
-  </head>
 
-  <body>
+</head>
+
+<body>
     <header>
         <nav class="nav_bar">
             <div class="logo">
@@ -26,30 +27,30 @@
             <div class="log_button">
                 <button><a href="../logout.php">SAIR</a></button>
             </div>
-        </nav> 
+        </nav>
     </header>
     <div class="espaço"></div>
 
 
     <div class="proj_code">
-      <div class="img_code">
-        <img class="img_teste" src="../../IMG/Img_TCC.jpg" alt="Imagem do protótipo">
+        <div class="img_code">
+            <img class="img_teste" src="../../IMG/Img_TCC.jpg" alt="Imagem do protótipo">
 
-      </div>
-      <div class="text_code">
-        <h1>CoDe: <p>Construção e desenvolvimento</p></h1>
-        <h3>O projeto <b>Code: Construção e Desenvolvimento</b> é um Trabalho de Conclusão de Curso-TCC
-         voltado para auxiliar os alunos da instituição qual encontransse dentro do quadro de 
-         Desigualde Tecnologica. Pois a cada 9 alunos por turma, três dos mesmo não possuem nenhum 
-         equipamento Tecnologica (Entre Desktop e Laptops), e os outros seis possuem somente um equipamento
-         qual necessitam dividir com seus familiares.
-        
+        </div>
+        <div class="text_code">
+            <h1>CoDe: <p>Construção e desenvolvimento</p>
+            </h1>
+            <h3>O projeto <b>Code: Construção e Desenvolvimento</b> é um Trabalho de Conclusão de Curso-TCC
+                voltado para auxiliar os alunos da instituição qual encontransse dentro do quadro de
+                Desigualde Tecnologica. Pois a cada 9 alunos por turma, três dos mesmo não possuem nenhum
+                equipamento Tecnologica (Entre Desktop e Laptops), e os outros seis possuem somente um equipamento
+                qual necessitam dividir com seus familiares.
+            </h3>
 
-        </h3>
-
-      </div>
+        </div>
 
     </div>
 
-  </body>
+</body>
+
 </html>
